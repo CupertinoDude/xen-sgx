ab6182cb16c156dfa3f167dcc60c84d6832a42f7
Kai Huang <kai.huang@linux.intel.com>
2016-07-04 20:52:19 +1200

    xen: x86: add early stage SGX feature detection
    This patch adds early stage SGX feature detection via SGX CPUID 0x12.
    Function detect_sgx is added to detect SGX info on each CPU (called from
    identify_cpu). SDM says SGX info returned by CPUID is per-thread, and
    we cannot assume all threads will return the same SGX info, so we have
    to detect SGX for each CPU.  For simplicity, currently SGX is only
    supported when all CPUs reports the same SGX info.
    
    Besides a boot parameter 'sgx' is added to allow the sysadmin control
    whether SGX is supported to guests.
    
    SDM also says it's possible to have multiple EPC sections but this is
    only for multiple-socket server, which we don't support now (there are
    other things need to be done, ex, NUMA EPC, scheduling, etc, as well),
    so currently only one EPC is supported.
    
    The detection result is in the X86_FEATURE_SGX bit of 'boot_cpu_data',
    and 'cpu_has_sgx' should be the only way to query for the SGX support
    enabled or not in the whole system.
    
    Dedicated files sgx.c and sgx.h are added for bulk of above SGX
    detection code detection code, and for further SGX code as well.
    
    Signed-off-by: Kai Huang <kai.huang@linux.intel.com>
    Signed-off-by: Boqun Feng <boqun.feng@intel.com>

---
+++ xen/include/asm-x86/sgx.h
@@ -0,0 +1,61 @@
+/*
+ * Intel Software Guard Extensions support
+ *
+ * Copyright (c) 2016-2017, Intel Corporation.
+ *
+ * Author: Kai Huang <kai.huang@linux.intel.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public
+ * License v2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program; If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef __ASM_X86_SGX_H__
+#define __ASM_X86_SGX_H__
+
+#include <xen/config.h>
+#include <xen/types.h>
+#include <xen/init.h>
+#include <asm/processor.h>
+
+#define SGX_CPUID 0x12
+
+/*
+ * SGX info reported by SGX CPUID.
+ *
+ * TODO:
+ *
+ * SDM (37.7.2 Intel SGX Resource Enumeration Leaves) actually says it's
+ * possible there are multiple EPC resources on the machine (CPUID.0x12,
+ * ECX starting with 0x2 enumerates available EPC resources until invalid
+ * EPC resource is returned). But this is only for multiple socket server,
+ * which we current don't support now (there are additional things need to
+ * be done as well). So far for simplicity we assume there is only one EPC.
+ */
+struct sgx_cpuinfo {
+#define SGX_CAP_SGX1    (1UL << 0)
+#define SGX_CAP_SGX2    (1UL << 1)
+    uint32_t cap;
+    uint32_t miscselect;
+    uint8_t max_enclave_size64;
+    uint8_t max_enclave_size32;
+    uint32_t secs_attr_bitmask[4];
+    uint64_t epc_base;
+    uint64_t epc_size;
+    bool lewr;
+};
+
+extern struct sgx_cpuinfo __read_mostly boot_sgx_cpudata;
+/* Detect SGX info for particular CPU via SGX CPUID */
+void detect_sgx(struct sgx_cpuinfo *sgxinfo);
+void disable_sgx(void);
+#define sgx_lewr() (boot_sgx_cpudata.lewr)
+
+#endif  /* __ASM_X86_SGX_H__ */
