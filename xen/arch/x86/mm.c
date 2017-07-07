b6277328099bc7774436875b781a911a4e1e722b
Kai Huang <kai.huang@linux.intel.com>
2017-07-08 10:36:04 +1200

    xen: x86/mm: introduce ioremap_wb()
    Currently Xen only has non-cacheable version of ioremap for x86.
    Although EPC is reported as reserved memory in e820 but it can be mapped
    as cacheable.  This patch introduces ioremap_wb() (ioremap for cacheable
    and write back memory).
    
    Signed-off-by: Kai Huang <kai.huang@linux.intel.com>
    Signed-off-by: Boqun Feng <boqun.feng@intel.com>

--- xen/arch/x86/mm.c
+++ xen/arch/x86/mm.c
@@ -5221,7 +5221,7 @@ void *__init arch_vmap_virt_end(void)
     return fix_to_virt(__end_of_fixed_addresses);
 }
 
-void __iomem *ioremap(paddr_t pa, size_t len)
+void __iomem *__ioremap(paddr_t pa, size_t len, unsigned int flags)
 {
     mfn_t mfn = _mfn(PFN_DOWN(pa));
     void *va;
@@ -5236,10 +5236,15 @@ void __iomem *ioremap(paddr_t pa, size_t len)
         unsigned int offs = pa & (PAGE_SIZE - 1);
         unsigned int nr = PFN_UP(offs + len);
 
-        va = __vmap(&mfn, nr, 1, 1, PAGE_HYPERVISOR_UCMINUS, VMAP_DEFAULT) + offs;
+        va = __vmap(&mfn, nr, 1, 1, flags, VMAP_DEFAULT) + offs;
     }
 
     return (void __force __iomem *)va;
+}
+
+void __iomem *ioremap(paddr_t pa, size_t len)
+{
+    return __ioremap(pa, len, PAGE_HYPERVISOR_UCMINUS);
 }
 
 int create_perdomain_mapping(struct domain *d, unsigned long va,
